a821a9f570da26d0acd2e0a79403059f36b06deb
dsreliete <dsreliete@gmail.com>
2019-12-02 11:59:09 -0500

    changed components structure: Presentational and Container Componentswas added

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -39,9 +39,11 @@ class CampsiteInfo extends Component {
         const campsite = this.props.campsite
         if(campsite) {
             return(
-                <div className="row">
-                    {this.renderCampsite(campsite)}
-                    {this.renderComments(campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(campsite)}
+                        {this.renderComments(campsite.comments)}
+                    </div>
                 </div>
             );
         }
